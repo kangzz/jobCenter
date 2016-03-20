2b0536433e4e511711ea0ee8235f19c2b6e1bf34
kangzz <zhuang_it@163.com>
2016-03-20 14:17:01 +0800

    fix

--- src/main/java/com/jobCenter/web/manger/UserAction.java
+++ src/main/java/com/jobCenter/web/manger/UserAction.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,8 +32,8 @@ public class UserAction {
 		//返回本机已通知成功标志
 
 		//System.out.println("id="+id);
-		User user = userService.getUserById("1");
-		System.out.println(user);
+		//User user = userService.getUserById("1");
+		//System.out.println(user);
 		return null;
 	}
 
